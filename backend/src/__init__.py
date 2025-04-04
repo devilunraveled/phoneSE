76b47800420ad56556ee098d8b3109a59bfd4cc9
Jaithecomder <jaib2910@gmail.com>
2025-04-04 17:56:15 +0530

    basic flask backend

---
+++ backend/src/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import os
+from src.config.config import Config
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# loading environment variables
+load_dotenv()
+
+# declaring flask application
+app = Flask(__name__)
+
+# calling the dev configuration
+config = Config().dev_config
+
+# making our application to use dev env
+app.env = config.ENV
+
+# Path for our local sql lite database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+# To specify to track modifications of objects and emit signals
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
+
+# sql alchemy instance
+db = SQLAlchemy(app)
+
+# Flask Migrate instance to handle migrations
+migrate = Migrate(app, db)
+
+# import models to let the migrate tool know
+# from src.models.user_model import User
---
+++ backend/src/routes.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+# main blueprint to be registered with application
+api = Blueprint('api', __name__)
